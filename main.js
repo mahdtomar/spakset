c33f239b999fe83fec2ffba639783ca9a204f8ca
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2023-04-20 17:59:32 +0200

    .

--- main.js
+++ main.js
@@ -3,17 +3,56 @@ const caption = document.querySelector(".before-after");
 const number = document.querySelector(".counter");
 const counterTargets = document.querySelectorAll(".observed");
 const background = document.querySelector("background");
+// const home = document.getElementById("home");
+// const prices = document.getElementById("prices");
+// const contacts = document.getElementById("contacts");
+const links = document.querySelectorAll(".navigator");
+const foodRocket = document.querySelector(".car-hero");
+const woodenPlate = document.getElementById("plate");
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    navigation(link);
+  });
+});
+
 const obseveTargets = [];
 obseveTargets.push(number);
+
 counterTargets.forEach((price) => {
   obseveTargets.push(price);
 });
+
+function navigation(e) {
+  id = e.getAttribute("data-target");
+  console.log(id);
+  targetElement = document.getElementById(id);
+  console.log(targetElement);
+  let scrollingSpace = targetElement.offsetTop;
+  console.log(scrollingSpace);
+  window.scrollTo({
+    top: scrollingSpace,
+    behavior: "smooth",
+  });
+}
 
 function increment(count, target, targervalue) {
   setInterval(() => {
     if (count < targervalue) {
       // count++;
-      targervalue > 1000 ? (count = count + 20) : count++;
+      switch (targervalue) {
+        case 1500:
+          count = count + 10;
+          break;
+        case 4000:
+          count = count + 22;
+          break;
+
+        default:
+          count = count + 3;
+          break;
+      }
+      // targervalue > 300 ? (count = count + 3) : count++;
+      // targervalue > 1500 ? (count = count + 20) : count++;
       target.innerText = count;
       if (target.innerText > targervalue) {
         target.innerText = targervalue;
@@ -21,10 +60,12 @@ function increment(count, target, targervalue) {
     }
   }, 10);
 }
+
 $(".background").click(function () {
   $(".nav_ul").removeClass("visible");
   $(".background").removeClass("visible");
 });
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     let count = 0;
@@ -40,20 +81,37 @@ const observer = new IntersectionObserver((entries) => {
 obseveTargets.forEach((target) => {
   observer.observe(target);
 });
+
+const movingObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.add("visible");
+    if (entry.isIntersecting) {
+      movingObserver.unobserve(entry.target);
+    }
+  });
+},{thresholds :.65});
+
+movingObserver.observe(foodRocket);
+movingObserver.observe(woodenPlate);
 let rootStyles = getComputedStyle(root);
+
 document.querySelector("#slider").addEventListener("input", (e) => {
   root.style.setProperty("--position", `${e.target.value}%`);
 });
+
 document.querySelector("#slider").addEventListener("mouseenter", (e) => {
   caption.classList.add("hidden");
 });
+
 document.querySelector("#slider").addEventListener("mouseleave", (e) => {
   caption.classList.remove("hidden");
 });
+
 $(".menu").click(function () {
   $(".nav_ul").addClass("visible");
   $(".background").addClass("visible");
 });
+
 $(".close-icon").click(function () {
   $(".background").removeClass("visible");
   $(".nav_ul").removeClass("visible");
